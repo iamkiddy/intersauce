6aba2ec45e33e7268232ba5f914aadc66e03596e
iamkiddy <francisyoungson@gmail.com>
2020-10-16 01:36:50 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>HELLO GUYS</h1>
+    <layout />
+  </div>
+</template>
+
+<script>
+import layout from "./components/layout";
+export default {
+  name: "App",
+  components: {
+    layout,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+</style>
